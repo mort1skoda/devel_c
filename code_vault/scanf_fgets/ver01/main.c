c8d2bff052793bf8b2e3d9596b0650b14fd80377
mort1skoda <mort1skoda@gmail.com>
2023-03-29 19:33:54 +0200

    sourcing: /home/m/.bash_aliases [2023-03-29 19:33:52] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

---
+++ code_vault/scanf_fgets/ver01/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "func1.h"
+#include "func2.h"
+#include "func3.h"
+
+// Morty Hawk 2023-03-29 Wed 18:16
+
+int main(int argc, char **argv) {
+    printf("asdf qwer\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("i=%d\n", i);
+    }
+
+	return 0;
+}
+
+
