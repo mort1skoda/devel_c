4d654287aac473f18d02aff53359e367c46a441d
mort1skoda <mort1skoda@gmail.com>
2023-03-30 05:15:40 +0200

    sourcing: /home/m/.bash_aliases [2023-03-30 05:15:37] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

---
+++ xlib/FTLabs/01creatingSimpleWindow/main.c
@@ -0,0 +1,21 @@
+#include "_01init.h"
+#include "func1.h"
+#include "func2.h"
+#include "func3.h"
+#include "get_string_length.h"
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>     // getenv(), etc.
+#include <string.h>
+#include <unistd.h>     // sleep(), etc.
+
+// Morty Hawk 2023-03-30 Thu 03:58
+
+int main(int argc, char **argv) {
+    Display* dpy;
+    static int scr;
+
+
+    return 0;
+}
+
