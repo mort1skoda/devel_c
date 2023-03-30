ed3b62539b3743c7c1672773d847f30bebc23597
mort1skoda <mort1skoda@gmail.com>
2023-03-30 02:31:21 +0200

    [2023-03-30 02:31:18]

---
+++ xlib/tronche/01openingDisplay/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "_01init.h"
+#include "func1.h"
+#include "func2.h"
+#include "func3.h"
+#include "get_string_length.h"
+// Morty Hawk 2023-03-29 Wed 22:54
+
+int main(int argc, char **argv) {
+    printf("01openingDisplay!");
+
+    return 0;
+}
+
