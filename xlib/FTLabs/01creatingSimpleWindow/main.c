0834ae28aaa0c462f570950d132001ba736da049
mort1skoda <mort1skoda@gmail.com>
2023-03-30 05:50:59 +0200

    sourcing: /home/m/.bash_aliases [2023-03-30 05:50:56] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

--- xlib/FTLabs/01creatingSimpleWindow/main.c
+++ xlib/FTLabs/01creatingSimpleWindow/main.c
@@ -1,3 +1,4 @@
+// include include list ...
 #include "_01init.h"
 #include "func1.h"
 #include "func2.h"
@@ -7,14 +8,53 @@
 #include <stdio.h>
 #include <stdlib.h>     // getenv(), etc.
 #include <string.h>
+#include <err.h>
 #include <unistd.h>     // sleep(), etc.
 
 // Morty Hawk 2023-03-30 Thu 03:58
 
-int main(int argc, char **argv) {
-    Display* dpy;
-    static int scr;
+static Display*  dpy;
+static int       scr;
+static Window    root;
 
+#define POSX    500
+#define POSY    500
+#define WIDTH   500
+#define HEIGHT  500
+#define BORDER   15
+
+int main(int argc, char **argv) {
+
+    Window win;
+    XEvent ev;
+
+    if ( (dpy = XOpenDisplay(NULL) ) == NULL )
+        err(1, "Can't open display");
+
+    scr  = DefaultScreen (dpy)      ;
+    root = RootWindow    (dpy, scr) ;
+
+    win  = 
+    XCreateSimpleWindow  (dpy,
+                         root,
+                         POSX, POSY,
+                         WIDTH, HEIGHT, BORDER,
+                         BlackPixel(dpy, scr),
+                         WhitePixel(dpy, scr)
+    );
+
+    XMapWindow           (dpy, win) ;
+
+    while (XNextEvent(dpy, &ev) == 0) {
+
+
+    }
+
+
+
+    XUnmapWindow         (dpy, win) ;
+    XDestroyWindow       (dpy, win) ;
+    XCloseDisplay        (dpy)      ;
 
     return 0;
 }
